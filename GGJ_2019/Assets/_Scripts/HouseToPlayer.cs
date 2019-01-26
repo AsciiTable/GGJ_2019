16f4cbc0c8eb7bff75dc52613ad0f1ef00843834
Alex Chheng <alexchheng1998@gmail.com>
2019-01-25 23:56:22 -0800

    New Tag "House" New Script HouseToPlayer for when player is in house range

---
+++ GGJ_2019/Assets/_Scripts/HouseToPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseToPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public float playerByHouse;
+    private Rigidbody2D rb2d;
+    private SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(gameObject.transform.position, player.transform.position);
+
+        if (distance < playerByHouse)
+        {
+            Debug.Log("Yes I am here");
+            spriteRenderer.color = new Color(1f, 1f, 1f, 0f);
+        }
+    }
+}
--- GGJ_2019/Assets/_Scripts/PlayerMovement.cs
+++ GGJ_2019/Assets/_Scripts/PlayerMovement.cs
@@ -5,32 +5,51 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
+    public float jump;
     public bool arcadeMovement;
 
     //used for the AddForce Movement
     private Rigidbody2D rb2d;
+    private bool isGround;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        isGround = false;
     }
 
     void FixedUpdate()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
 
         //Used for the AddForce movement;
-        Vector2 movement = new Vector2(horizontal, vertical);
+        //This code have a bug (transform.position.y) Do not know what it does specifically. Bug is if player jump a certain height it would go on forever.
+        Vector2 movement = new Vector2(horizontal, transform.position.y);
 
         //Movement selection by the boolean
         if (arcadeMovement)
         {
-            transform.position += new Vector3(horizontal * speed, vertical * speed, transform.position.z);
+            //y position have a bug
+            transform.position += new Vector3(horizontal * speed, 0, transform.position.z);
         }
         else if (!arcadeMovement)
         {
             rb2d.AddForce(movement * speed);
+        }
+
+        if (Input.GetKeyDown("up") && isGround)
+        {
+            Debug.Log("Up is press");
+            isGround = false;
+            rb2d.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGround = true;
         }
     }
 }

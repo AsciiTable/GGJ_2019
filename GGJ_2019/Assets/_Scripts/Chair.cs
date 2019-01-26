f23770e007dd039f694e174321ebeb6de373075b
jusin-PC\Just <justincheung6@gmail.com>
2019-01-26 08:31:17 -0800

    I just woke up, forgot what I added

---
+++ GGJ_2019/Assets/_Scripts/Chair.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chair : MonoBehaviour
+{
+    public GameObject player;
+    public float objectClosenesss;
+    private Rigidbody2D rb2d;
+    private Animator ChairAnim;
+
+    private bool sitable = false;
+    public bool sitting = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        ChairAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Interact"))
+        {
+            if(sitting == true)
+            {
+                sitting = false;
+            }
+            else if(sitting == false)
+            {
+                sitting = true;
+            }
+
+            ChairAnim.SetTrigger("ToggleSit");
+        }
+
+        float distance = Vector3.Distance(gameObject.transform.position, player.transform.position);
+
+        if (distance < objectClosenesss)
+        {
+            Debug.Log("Yes I am here");
+            sitable = true;
+        }
+        else
+        {
+            sitable = false;
+        }
+
+        
+
+    }
+}
---
+++ GGJ_2019/Assets/_Scripts/SceneTransition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    //Load Scene with specific index
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    //Load Scene next in list (next level)
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //Load Scene previous in list (previous part of level)
+    public void PrevScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    //Leave Game Application
+    public void LeaveGame() 
+    {
+        Debug.Log("Leave Game");
+        Application.Quit();
+    }
+}

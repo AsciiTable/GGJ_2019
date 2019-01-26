98e1409b32316dbd312806b90a398e5a53f0e5a4
Alex Chheng <alexchheng1998@gmail.com>
2019-01-26 14:40:15 -0800

    Platform script and scene

---
+++ GGJ_2019/Assets/_Scripts/PlatformStep.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformStep : MonoBehaviour
+{
+    //public bool isPlayerOn;
+    private PlatformEffector2D effector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        effector = GetComponent<PlatformEffector2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            effector.rotationalOffset = 0f;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            effector.rotationalOffset = 180f;
+        }
+    }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if(collision.gameObject.tag == "Player")
+    //    {
+    //        isPlayerOn = true;
+    //    }
+    //}
+
+    //private void OnCollisionExit2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        isPlayerOn = false;
+    //    }
+    //}
+}

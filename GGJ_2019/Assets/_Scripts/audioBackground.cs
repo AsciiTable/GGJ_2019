38afde4089e92139866c943d3daa06f3b00e68c6
Alex Chheng <alexchheng1998@gmail.com>
2019-01-26 15:15:16 -0800

    audio Manager and script

---
+++ GGJ_2019/Assets/_Scripts/audioBackground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioBackground : MonoBehaviour
+{
+    public AudioSource backgroundCave;
+    public AudioSource backgroundFirst;
+    public AudioSource backgroundSecond;
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgroundCave.loop = true;
+        backgroundCave.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

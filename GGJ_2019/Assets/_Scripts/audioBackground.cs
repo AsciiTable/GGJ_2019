f47ec6ae34aecbe817f269d55345376986158230
Alex Chheng <alexchheng1998@gmail.com>
2019-01-26 15:37:20 -0800

    audioManager lvl up Audio Script edit audio scene add

--- GGJ_2019/Assets/_Scripts/audioBackground.cs
+++ GGJ_2019/Assets/_Scripts/audioBackground.cs
@@ -7,16 +7,45 @@ public class audioBackground : MonoBehaviour
     public AudioSource backgroundCave;
     public AudioSource backgroundFirst;
     public AudioSource backgroundSecond;
+
+    public int audioNumber;
     // Start is called before the first frame update
     void Start()
     {
         backgroundCave.loop = true;
-        backgroundCave.Play();
+        backgroundFirst.loop = true;
+        backgroundSecond.loop = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            audioNumber += 1;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            audioNumber -= 1;
+        }
+
+        if(audioNumber == 0 && !backgroundCave.isPlaying)
+        {
+            backgroundCave.Play();
+            backgroundFirst.Stop();
+            backgroundSecond.Stop();
+        }
+        else if (audioNumber == 1 && !backgroundFirst.isPlaying)
+        {
+            backgroundCave.Stop();
+            backgroundFirst.Play();
+            backgroundSecond.Stop();
+        }
+        else if (audioNumber == 2 && !backgroundSecond.isPlaying)
+        {
+            backgroundCave.Stop();
+            backgroundFirst.Stop();
+            backgroundSecond.Play();
+        }
     }
 }

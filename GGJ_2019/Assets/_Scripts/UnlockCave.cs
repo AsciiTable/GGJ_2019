4c478295b6a36c6d096cd88dc1d576383bc70aca
jusin-PC\Just <justincheung6@gmail.com>
2019-01-26 18:08:15 -0800

    Finished Cave Scene ish

---
+++ GGJ_2019/Assets/_Scripts/UnlockCave.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class UnlockCave : MonoBehaviour
+{
+    private WorldStatus worldStatus;
+    private CinemachineVirtualCamera cinemachine;
+    private GameObject tempBlock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        worldStatus = GameObject.FindGameObjectWithTag("ScriptHolder").GetComponent<WorldStatus>();
+        tempBlock = GameObject.Find("TempBlock");
+
+        cinemachine = GetComponent<CinemachineVirtualCamera>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (worldStatus.DetailedBackground == true)
+        {
+            cinemachine.enabled = true;
+
+            if(tempBlock)
+            {
+                Destroy(tempBlock.gameObject);
+            }
+        }
+    }
+}

64a6967d0c65e56be6cb016cd411f27175ff8136
jusin-PC\Just <justincheung6@gmail.com>
2019-01-27 16:49:02 -0800

    Added Granny Animation

---
+++ GGJ_2019/Assets/_Scripts/Backgrounds/GrannyColor.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrannyColor : MonoBehaviour
+{
+    private WorldStatus worldStatus;
+    private Animator animator;
+
+    private float change = 0f;
+    private float timer = 0;
+    private bool activated = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        worldStatus = GameObject.FindGameObjectWithTag("ScriptHolder").GetComponent<WorldStatus>();
+        animator = GetComponent<Animator>();
+
+        if(worldStatus.CaveBackground == true)
+        {
+            activated = true;
+            animator.SetTrigger("Done");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activated == false)
+        {
+            if (worldStatus.GrandfatherClock && change < 3)
+            {
+                timer += Time.deltaTime;
+                animator.SetTrigger("addColor");
+            }
+
+            if (timer >= 2f && change == 0)
+            {
+                change++;
+            }
+            if (change == 1)
+            {
+                animator.enabled = false;
+            }
+        }
+    }
+}
